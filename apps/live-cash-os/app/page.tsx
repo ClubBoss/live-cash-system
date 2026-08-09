70222d34f7314c3f2d2287f7b5577300fa598bb6
ClubBoss <esalimzade@hotmail.com>
2026-08-09 05:25:21 +0400

    feat(g4): compose real-use learning integrity layer

--- apps/live-cash-os/app/page.tsx
+++ apps/live-cash-os/app/page.tsx
@@ -1,11 +1,13 @@
 import LiveCashApp from "../components/LiveCashApp";
 import Wave5PracticeLayer from "../components/Wave5PracticeLayer";
 import Wave8AccessibilityLayer from "../components/Wave8AccessibilityLayer";
+import Gauntlet4LearningIntegrityLayer from "../components/Gauntlet4LearningIntegrityLayer";
 
 export default function Home() {
   return <>
     <LiveCashApp />
     <Wave5PracticeLayer />
     <Wave8AccessibilityLayer />
+    <Gauntlet4LearningIntegrityLayer />
   </>;
 }
