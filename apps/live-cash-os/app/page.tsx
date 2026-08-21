5493c08fb9e1de2adfe681cc95337c895d09df41
ClubBoss <esalimzade@hotmail.com>
2026-08-21 16:35:34 +0400

    D: connect canonical home to Practical Mastery

--- apps/live-cash-os/app/page.tsx
+++ apps/live-cash-os/app/page.tsx
@@ -1,4 +1,5 @@
 import LiveCashApp from "../components/LiveCashApp";
+import PracticalMasteryGateway from "../components/PracticalMasteryGateway";
 import Wave5PracticeLayer from "../components/Wave5PracticeLayer";
 import Wave8AccessibilityLayer from "../components/Wave8AccessibilityLayer";
 import Gauntlet4LearningIntegrityLayer from "../components/Gauntlet4LearningIntegrityLayer";
@@ -8,6 +9,7 @@ import TestInviteGate from "../components/TestInviteGate";
 
 export default function Home() {
   return <TestInviteGate>
+    <PracticalMasteryGateway />
     <LiveCashApp />
     <Wave5PracticeLayer />
     <Wave8AccessibilityLayer />
